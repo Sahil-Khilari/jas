bb4a831afc5af72cf3850b9bb74bff6e328b0fff
Sahil-Khilari <khilarisahil786@gmail.com>
2025-06-19 18:51:35 +0530

    ok

--- basics/test1.js
+++ basics/test1.js
@@ -1,1 +1,8 @@
 const accountName = "nickolas";
+let accountState = "working"
+let accountNumber =23410428
+console.table([accountName,accountState,accountNumber]);
+/*
+prefer not to use var because of block scope 
+and functional scope :)
+*/
